b7c905941d1cbabe688cc18db68f67222229df2f
thomasgsquinaia <thomasquinaia@gmail.com>
2023-05-11 16:04:10 -0300

    coloquei cors no middleware e funcionou as requests no frontend

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const app = express();
-const port = 3333;
+const cors = require('cors')
+const port = 3001;
 const bodyparser = require("body-parser");
 app.use(bodyparser.json({ limit: "50mb" }));
 //ROUTERS
 const notes = require("./routes/noteRoute");
 
 //NOT-AUTHORIZATION
-app.use("/", notes);
+app.use("/notes",cors(), notes);
 
 app.listen(port, () => {
-    console.log(`Rodando na porta ${port}`);
+    console.log(`Rodando na porta http://localhost:${port}`);
 });
 
 app.use(function(req, res, next) {
@@ -19,12 +20,12 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, contentType, Content-Type, Accept, Authorization');
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, PUT, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, contentType, Content-Type, Accept, Authorization');
+//     next();
+// });
 
 
 module.exports = app;
